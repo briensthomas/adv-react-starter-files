4ed83543c76f4e3ec06628f24edfd4f420d21712
Brien Thomas <thomasbrien94@gmail.com>
2022-09-27 10:25:47 -0700

    making change for remote repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, 
-  Routes, 
-  Route, 
-  NavLink } from 'react-router-dom';
+import { BrowserRouter as Router,  } from 'react-router-dom';
 
 export default function App() {
   return (
     <Router>
-        <Routes>
-            <Route element={<Layout />}>
 
-            <Route />
-        </Routes>
     </Router>
   );
 }
