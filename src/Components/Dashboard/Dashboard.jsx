4600d3f9ce338ba1267e1f7aef2a9558184f9f5c
Brien Thomas <thomasbrien94@gmail.com>
2022-09-27 17:29:03 -0700

    basic form and elements created based on screenshot

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -4,6 +4,50 @@ export default function Dashboard() {
       <h2>
         Hello World
       </h2>
+
+      <form>
+        <label htmlFor="name">Name
+          <input name="name" value={name} placeholder="Enter your name"/>
+        </label>
+
+        <label>
+          Date
+          <input type="date" name="date" />
+
+        </label>
+
+        <label>
+          Option
+          <select required>
+            <option selected disabled>Choose One</option>
+            <option>A</option>
+            <option>B</option>
+            <option>C</option>
+          </select>
+        </label>
+
+        <label>
+          Bio
+          <textarea name="bio" placeholder="From whence do you come" />
+        </label>
+
+        <fieldset>
+          <legend>
+            Do you agree to the terms and conditions?
+          </legend>
+          <label>
+            Yes
+            <input type="checkbox" />
+          </label>
+          <label>
+            No
+            <input type="checkbox" />
+          </label>
+        </fieldset>
+        <button>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
